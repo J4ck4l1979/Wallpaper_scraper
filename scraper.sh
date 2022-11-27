70d55ea6f0a08ed6f5b300ff8b15ed17f1a2133e
J4ck4l1979 <104032464+J4ck4l1979@users.noreply.github.com>
2022-11-27 23:17:39 +0000

    Update scraper.sh
    Adding searches for "toplist" 10 pages all wide resolution.

--- scraper.sh
+++ scraper.sh
@@ -5,7 +5,7 @@
 page=1; while [ $page -lt 10 ];
 
    do 
-     curl -s "https://wallhaven.cc/api/v1/search?q=lain&page=$page" | jq '.data[].path' | xargs -I{} wget {};     
+     curl -s "https://wallhaven.cc/api/v1/search?categories=111&purity=111&ratios=landscape&topRange=1M&sorting=toplist&order=desc&page=$page" | jq '.data[].path' | xargs -I{} wget {};     
        page=$(( page+1 ));
 
    done
